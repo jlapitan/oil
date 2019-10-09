09df34e59a5295df26b309b29a200f5b95a2525e
Collyn Philleo <collyn.philleo@automattic.com>
2019-10-09 15:43:00 -0700

    Streamline for smaller initial page loading
    - Removed some methods on the global Oil object
    - Removed preview mode
    - Inlined fast check for opt-in cookie, to reduce page load for already opted-in scenario

--- src/scripts/core/core_oil.js
+++ src/scripts/core/core_oil.js
@@ -1,10 +1,8 @@
 import { OilVersion, sendEventToHostSite, setGlobalOilObject } from './core_utils';
-import { logError, logInfo, logPreviewInfo } from './core_log';
-import { checkOptIn } from './core_optin';
-import { getSoiCookie, isBrowserCookieEnabled, isPreviewCookieSet, removePreviewCookie, removeVerboseCookie, setPreviewCookie, setVerboseCookie } from './core_cookies';
-import { getLocale, isPreviewMode, resetConfiguration, setGdprApplies } from './core_config';
+import { logError, logInfo} from './core_log';
+import { getLocale} from './core_config';
 import { EVENT_NAME_HAS_OPTED_IN, EVENT_NAME_NO_COOKIES_ALLOWED } from './core_constants';
-import { executeCommandCollection } from './core_command_collection';
+import Cookie from 'js-cookie';
 
 /**
  * Initialize Oil on Host Site
@@ -13,63 +11,94 @@ import { executeCommandCollection } from './core_command_collection';
 export function initOilLayer() {
   logInfo(`Init OilLayer (version ${OilVersion.get()})`);
 
-  if (isPreviewMode() && !isPreviewCookieSet()) {
-    logPreviewInfo('Preview mode ON and OIL layer remains hidden. Run AS_OIL.previewModeOn() and reload to display the layer.');
-  }
-
   attachUtilityFunctionsToWindowObject();
 
   /**
-   * We show OIL depending on the following conditions:
-   * With Dev Mode turned on, we only show Oil if a developer cookie is set
+   * Cookies are not enabled
    */
-  if (!isPreviewMode() || isPreviewCookieSet()) {
-    /**
-     * Cookies are not enabled
-     */
-    if (!isBrowserCookieEnabled()) {
-      logInfo('This browser doesn\'t allow cookies.');
+  if (!isBrowserCookieEnabled()) {
+    logInfo('This browser doesn\'t allow cookies.');
+    import('../userview/locale/userview_oil.js')
+      .then(userview_modal => {
+        userview_modal.locale(uv_m => uv_m.renderOil({noCookie: true}));
+      })
+      .catch((e) => {
+        logError('Locale could not be loaded.', e);
+      });
+    sendEventToHostSite(EVENT_NAME_NO_COOKIES_ALLOWED);
+    return;
+  }
+
+  /**
+   * We read our cookie and get an opt-in value, true or false
+   */
+  quickCheckOptIn().then((optin) => {
+    if (optin) {
+      /**
+       * User has opted in
+       */
+      import('./core_command_collection')
+        .then(module => {
+
+          sendEventToHostSite(EVENT_NAME_HAS_OPTED_IN);
+          module.executeCommandCollection();
+          attachCommandCollectionFunctionToWindowObject(module.executeCommandCollection);
+      });
+    } else {
+      /**
+       * Any other case, when the user didn't decide before and oil needs to be shown:
+       */
       import('../userview/locale/userview_oil.js')
         .then(userview_modal => {
-          userview_modal.locale(uv_m => uv_m.renderOil({noCookie: true}));
+          userview_modal.locale(uv_m => uv_m.renderOil({optIn: false}));
+          import('./core_command_collection')
+            .then(module => {
+              attachCommandCollectionFunctionToWindowObject(module.executeCommandCollection);
+            });
         })
         .catch((e) => {
           logError('Locale could not be loaded.', e);
         });
-      sendEventToHostSite(EVENT_NAME_NO_COOKIES_ALLOWED);
-      return;
     }
-
-    /**
-     * We read our cookie and get an opt-in value, true or false
-     */
-    checkOptIn().then((optin) => {
-      if (optin) {
-        /**
-         * User has opted in
-         */
-        sendEventToHostSite(EVENT_NAME_HAS_OPTED_IN);
-        executeCommandCollection();
-        attachCommandCollectionFunctionToWindowObject();
-      } else {
-        /**
-         * Any other case, when the user didn't decide before and oil needs to be shown:
-         */
-        import('../userview/locale/userview_oil.js')
-          .then(userview_modal => {
-            userview_modal.locale(uv_m => uv_m.renderOil({optIn: false}));
-            attachCommandCollectionFunctionToWindowObject();
-          })
-          .catch((e) => {
-            logError('Locale could not be loaded.', e);
-          });
-      }
-    });
-  }
+  });
 }
 
-function attachCommandCollectionFunctionToWindowObject() {
-  setGlobalOilObject('commandCollectionExecutor', executeCommandCollection);
+function isBrowserCookieEnabled() {
+  Cookie.set('oil_cookie_exp', 'cookiedata');
+  let result = isCookie('oil_cookie_exp');
+  Cookie.remove('oil_cookie_exp');
+  return result;
+}
+
+function isCookie(name) {
+  return typeof (Cookie.get(name)) !== 'undefined';
+}
+
+function quickCheckOptIn() {
+  return new Promise((resolve) => {
+    let soiOptIn = getOilCookieOptIn();
+
+    if (soiOptIn) {
+      logInfo('User has given SOI permit, OIL not shown.');
+    } else {
+      logInfo('User has not opted in at all, OIL should be shown.');
+    }
+
+    resolve(soiOptIn);
+  });
+}
+
+function getOilCookieOptIn() {
+  if (isCookie('oil_data')) {
+    let cookieJson = Cookie.getJSON('oil_data');
+    return cookieJson.opt_in;
+  }
+
+  return false;
+}
+
+function attachCommandCollectionFunctionToWindowObject(callback) {
+  setGlobalOilObject('commandCollectionExecutor', callback);
 }
 
 /**
@@ -91,47 +120,10 @@ function attachUtilityFunctionsToWindowObject() {
       });
   }
 
-  setGlobalOilObject('previewModeOn', () => {
-    setPreviewCookie();
-    return 'preview mode on';
-  });
-
-  setGlobalOilObject('previewModeOff', () => {
-    removePreviewCookie();
-    return 'preview mode off';
-  });
-
-  setGlobalOilObject('verboseModeOn', () => {
-    setVerboseCookie();
-    return 'verbose mode on';
-  });
-
-  setGlobalOilObject('verboseModeOff', () => {
-    removeVerboseCookie();
-    return 'verbose mode off';
-  });
-
-  setGlobalOilObject('reload', () => {
-    resetConfiguration();
-    initOilLayer();
-    return 'OIL reloaded';
-  });
-
-  setGlobalOilObject('status', () => {
-    return getSoiCookie();
-  });
-
   setGlobalOilObject('showPreferenceCenter', () => {
     loadLocale(userview_modal => {
       userview_modal.oilShowPreferenceCenter();
     });
   });
 
-  setGlobalOilObject('applyGDPR', () => {
-    setGdprApplies(true);
-    initOilLayer();
-    return 'GDPR applied';
-  });
 }
-
-
