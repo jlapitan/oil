43b28be500d035d6ef89a25deb09eb4e870e9da4
Collyn Philleo <collyn.philleo@automattic.com>
2019-11-21 16:00:48 -0800

    Rename oil-configuration to cmp-configuration

--- src/public_path.js
+++ src/public_path.js
@@ -1,7 +1,7 @@
 if (window.AS_OIL && window.AS_OIL.CONFIG) {
   __webpack_public_path__ = window.AS_OIL.CONFIG.publicPath;
 } else {
-  const configurationElement = document.querySelector('script[type="application/configuration"]#oil-configuration');
+  const configurationElement = document.querySelector('script[type="application/configuration"]#cmp-configuration');
   if (configurationElement !== null && configurationElement.text) {
     try {
       let parsedConfig = JSON.parse(configurationElement.text);
